59f0db2f968fdb7e3747a2110460855386297d43
unknown <ibrahimkhalil.mine@gmail.com>
2022-02-14 11:37:59 +0600

    done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
-function addItem(isAdd) {
-    const itemInput = document.getElementById("item-quantity-input");
+function addItem(product, price, isAdd) {
+    const itemInput = document.getElementById(product + "-quantity-input");
     const itemInputText = itemInput.value;
     let itemInputNumber = parseInt(itemInputText);
 
@@ -11,16 +11,40 @@ function addItem(isAdd) {
         itemInput.value = itemInputNumber - 1;
     }
     itemInputNumber = itemInput.value
-    const itemPrice = document.getElementById("item-price");
-    itemPrice.innerText = 1219 * itemInputNumber
+    const itemPrice = document.getElementById(product + "-price");
+    itemPrice.innerText = price * itemInputNumber;
+
+    document.getElementById("subtotal").innerText = calculation()
+
+    document.getElementById("tex").innerText = calculation() * 0.1;
+
+    document.getElementById("grand-total").innerText = calculation() + texAmoutn;
+
+
 }
 
+function calculation() {
+    const PhonePrice = document.getElementById("phone-price").innerText;
+    const casePrice = document.getElementById("case-price").innerText;
+    const subtotal = parseFloat(PhonePrice) + parseFloat(casePrice);
+    return subtotal;
+}
 
+// phone section 
 
-document.getElementById("plus-button").addEventListener("click", function () {
-    addItem(true)
+document.getElementById("phone-plus").addEventListener("click", function () {
+    addItem("phone", 1219, true)
 })
 
-document.getElementById("minus-button").addEventListener("click", function () {
-    addItem(false)
+document.getElementById("phone-minus").addEventListener("click", function () {
+    addItem("phone", 1219, false)
+
+})
+
+//case section
+document.getElementById("case-plus").addEventListener("click", function () {
+    addItem("case", 59, true)
+})
+document.getElementById("case-minus").addEventListener("click", function () {
+    addItem("case", 59, false)
 })
